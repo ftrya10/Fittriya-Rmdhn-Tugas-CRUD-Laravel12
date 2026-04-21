868b5ad5b8a8822113c353331430242de5267801
fittriya ramadhani <fittriya.3gcell3@gmail.com>
2026-04-21 17:14:58 +0700

    crud salon selesai

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -4,8 +4,21 @@
     @csrf
 
     <input type="text" name="nama_pelanggan" placeholder="Nama Pelanggan"><br><br>
-    <input type="text" name="layanan" placeholder="Layanan"><br><br>
+
+    <select name="layanan">
+        <option value="">-- Pilih Layanan --</option>
+        <option value="Hair Spa">Hair Spa</option>
+        <option value="Creambath">Creambath</option>
+        <option value="Potong Rambut">Potong Rambut</option>
+        <option value="Hair Coloring">Hair Coloring</option>
+        <option value="Facial">Facial</option>
+        <option value="Manicure">Manicure</option>
+        <option value="Pedicure">Pedicure</option>
+        <option value="Smoothening">Smoothening</option>
+    </select><br><br>
+
     <input type="number" name="harga" placeholder="Harga"><br><br>
+
     <input type="date" name="tanggal_booking"><br><br>
 
     <button type="submit">Simpan</button>
